0f01a265418dd910d23684050d9e8b62a80b18f2
yinnan-996 <1301142455@qq.com>
2018-11-20 21:45:45 +0800

    添加基础spring-boot
    添加基础spring-boot

---
+++ src/main/java/com/yinnan/progress/LearnProgressTogetherApplication.java
@@ -0,0 +1,12 @@
+package com.yinan.progress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnProgressTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnProgressTogetherApplication.class, args);
+    }
+}
---
+++ src/test/java/com/yinan/progress/LearnProgressTogetherApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yinan.progress;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnProgressTogetherApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
